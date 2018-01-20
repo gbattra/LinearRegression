c96cd9c544df03608c8e7873feb0134c1b783943
Greg Attra <greg.attra@gmail.com>
2018-01-20 16:41:35 -0500

    finished the multi variate linear regression

--- linear_regression_multi.py
+++ linear_regression_multi.py
@@ -1,6 +1,94 @@
+# this is a multivariate variable linear regression model
+# it predicts the number of expected bike rentals based on the temperature, humidity and windspeed for that day
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datacleaner
 from compute_cost import compute_cost
 from gradient_descent import gradient_descent
+
+# import the data
+data = pd.read_csv('dataset_multi.csv')
+clean_data = datacleaner.autoclean(data, True).values
+X = np.matrix(clean_data[:, 0:3])
+y = np.matrix(clean_data[:, 3:4])
+
+# plot the data
+plt.plot(X[:, 0], y, 'ro')
+plt.plot(X[:, 1], y, 'yo')
+plt.plot(X[:, 2], y, 'bo')
+
+# add ones
+X0 = np.ones((X.shape[0], 1))
+X = np.hstack((X0, X))
+
+# initialize thetas
+theta = np.zeros((X.shape[1], 1))
+
+# initialize learning parameters
+alpha = 0.1
+iterations = 100
+
+# test cost function
+J = compute_cost(X, y, theta)
+
+# run gradient descent
+[theta, J_history] = gradient_descent(X, y, theta, alpha, iterations)
+
+# plot J_history to check that gradient descent worked
+plt.plot(range(iterations), J_history)
+
+# test hypothesis
+temperature = 0.65
+windspeed = 0.22
+humidity = 0.6
+x = np.matrix([1, temperature, windspeed, humidity])
+h = x.dot(theta).item(0)
+
+print('No. of probable bike rentals: ' + str(h))
+# this is a multivariate variable linear regression model
+# it predicts the number of expected bike rentals based on the temperature, humidity and windspeed for that day
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datacleaner
+from compute_cost import compute_cost
+from gradient_descent import gradient_descent
+
+# import the data
+data = pd.read_csv('dataset_multi.csv')
+clean_data = datacleaner.autoclean(data, True).values
+X = np.matrix(clean_data[:, 0:3])
+y = np.matrix(clean_data[:, 3:4])
+
+# add ones
+X0 = np.ones((X.shape[0], 1))
+X = np.hstack((X0, X))
+
+# initialize thetas
+theta = np.zeros((X.shape[1], 1))
+
+# initialize learning parameters
+alpha = 0.1
+iterations = 100
+
+# test cost function
+J = compute_cost(X, y, theta)
+
+# run gradient descent
+[theta, J_history] = gradient_descent(X, y, theta, alpha, iterations)
+
+# plot J_history to check that gradient descent worked
+plt.cla()
+plt.plot(range(iterations), J_history)
+
+# test hypothesis
+temperature = 0.65
+windspeed = 0.22
+humidity = 0.6
+x = np.matrix([1, temperature, windspeed, humidity])
+h = x.dot(theta).item(0)
+
+print('No. of probable bike rentals: ' + str(h))
--- linear_regression_single.py
+++ linear_regression_single.py
@@ -1,3 +1,6 @@
+# this is a single variable linear regression model
+# it predicts the number of expected bike rentals based on the temperature for that day
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,25 +8,24 @@ import datacleaner
 from compute_cost import compute_cost
 from gradient_descent import gradient_descent
 
-# this is a basic linear regression model
-# it predicts the number of expected bike rentals based on the temperature for that day
+# read data
 data = pd.read_csv('dataset_linear.csv')
 clean_data = datacleaner.autoclean(data, True).values
 X = np.matrix(clean_data[:, 0:1])
 y = np.matrix(clean_data[:, 1:2])
 
 # plot the data
-plt.plot(X, y, 'ro')
+plt.plot(X[:, 0], y, 'ro')
 
 # add ones
-X0 = np.ones((X.size, 1))
+X0 = np.ones((X.shape[0], 1))
 X = np.hstack((X0, X))
 
 # initialize thetas
 theta = np.zeros((X.shape[1], 1))
 
 # initialize learning parameters
-iterations = 100
+iterations = 1000
 alpha = 1.4
 
 # test cost function
@@ -41,21 +43,3 @@ plt.cla()
 plt.xlim(0, x.max())
 plt.scatter(clean_data[:, 0:1], clean_data[:, 1:2])
 plt.plot(x, h)
-
-# quadratic model training
-alpha = 1.4
-iterations = 200
-X = np.matrix(clean_data[:, 0:1])
-X0 = np.ones((X.size, 1))
-X2 = np.square(X)
-X = np.hstack((X0, X, X2))
-theta = np.zeros((X.shape[1], 1))
-[theta, J_history_quadratic] = gradient_descent(X, y, theta, alpha, iterations)
-
-# quadratic model plot
-x_init = np.matrix(np.arange(0, 1, 0.01)).T
-x0 = np.ones((x_init.size, 1))
-x2 = np.square(x_init)
-x = np.hstack((x0, x_init, x2))
-h = x.dot(theta)
-plt.plot(x_init, h, 'red')
