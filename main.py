9975eb360ebf6461c9ad2a92bd90d5791c619bd9
Greg Attra <greg.attra@gmail.com>
2018-01-20 14:09:43 -0500

    finished the linear regression model

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
-import math
-import scipy
 import datacleaner
 from compute_cost import compute_cost
 from gradient_descent import gradient_descent
 
 # this is a basic linear regression model
-# it predicts the length of the bike trip by the age of the rider
+# it predicts the number of expected bike rentals based on the temperature for that day
 data = pd.read_csv('dataset_linear.csv')
 clean_data = datacleaner.autoclean(data, True).values
 X = np.matrix(clean_data[:, 0:1])
 y = np.matrix(clean_data[:, 1:2])
-
-# preprocess X (convert it from year to age)
-for i in range(0, X.size):
-    X[i] = datetime.datetime.now().year - X[i]
-
 
 # plot the data
 plt.plot(X, y, 'ro')
@@ -33,7 +25,7 @@ theta = np.zeros((X.shape[1], 1))
 
 # initialize learning parameters
 iterations = 100
-alpha = 0.000000001
+alpha = 0.1
 
 # test cost function
 J = compute_cost(X, y, theta)
@@ -41,5 +33,13 @@ J = compute_cost(X, y, theta)
 # train Thetas to fit data
 [theta, J_history] = gradient_descent(X, y, theta, alpha, iterations)
 
-h = X.dot(theta)
+# plot hypothesis on top scatter of data
+x = np.matrix(np.arange(0, 1, 0.01)).T
+x0 = np.ones((x.size, 1))
+x = np.hstack((x0, x))
+h = x.dot(theta)
+plt.cla()
+plt.xlim(0, x.max())
+plt.scatter(clean_data[:, 0:1], clean_data[:, 1:2])
+plt.plot(x, h)
 
